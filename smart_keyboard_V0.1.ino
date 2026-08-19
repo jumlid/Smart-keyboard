c756d2f3dd586f94bb8d637b1b74dd3309693ed6
boi <148221429+jumlid@users.noreply.github.com>
2026-08-20 01:33:47 +0530

    Added a web UI
    Added a web UI that can control the esp32 inbuild LED just for demo

--- smart_keyboard_V0.1.ino
+++ smart_keyboard_V0.1.ino
@@ -1,17 +1,54 @@
 #include <WiFi.h>
+#include <WebServer.h>
 const char* ssid = "Smart Keyboard";
-const int led =2;
+const int led = 2;
+WebServer server(80);  // this line open a server in port 80
 void setup() {
-pinMode(led,OUTPUT);
-  WiFi.softAP(ssid);
-  
+  pinMode(led, OUTPUT);
+  WiFi.softAP(ssid);  // This method is use to create a wifi network it uses two parameter (ssid,password) SSID refer to the network name and password while password can be emty for no password
+  server.on("/", []() {
+    server.send(200, "text/html", R"rawliteral(
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>ESP32</title>
+      </head>
+      <body>
+        <h1>Hello from ESP32!</h1>
+        <button onclick="turnOn()">ON</button>
+           <button onclick="turnOff()">OFF</button>
+         <script>
+        function turnOn() {
+            fetch("Mode1")
+                .then(response => response.text())
+                .then(data => {
+                    console.log(data);
+                });
+        }
+
+        function turnOff() {
+            fetch("/Mode2")
+                .then(response => response.text())
+                .then(data => {
+                });
+        }
+    </script>
+      </body>
+      </html>
+    )rawliteral");
+  });
+  server.on("/Mode1", []() {
+    digitalWrite(led, HIGH);
+    server.send(200, "text/plain", "LED is ON");
+  });
+
+server.on("/Mode2", []() {
+  digitalWrite(led, LOW);
+  server.send(200, "text/plain", "LED is OFF");
+});
+server.begin();
 }
 
 void loop() {
-  if (WiFi.softAPgetStationNum() > 0) {
-    digitalWrite(led, HIGH);
-  }else {
-    digitalWrite(led, LOW);
-  }
-
+  server.handleClient();
 }
