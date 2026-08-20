cccfd15ea2292a53d5178f88975e265be048ff7d
boi <148221429+jumlid@users.noreply.github.com>
2026-08-21 01:17:02 +0530

    updated the logic

--- smart_keyboard_V1.ino
+++ smart_keyboard_V1.ino
@@ -2,9 +2,12 @@
 #include <WebServer.h>
 const char* ssid = "Smart Keyboard";
 const int led = 2;
+const int keyboard = 15;
+ bool mode_3 = false;
 WebServer server(80);  // this line open a server in port 80
 void setup() {
   pinMode(led, OUTPUT);
+  pinMode(keyboard, OUTPUT);
   WiFi.softAP(ssid);  // This method is use to create a wifi network it uses two parameter (ssid,password) SSID refer to the network name and password while password can be emty for no password
   server.on("/", []() {
     server.send(200, "text/html", R"rawliteral(<!DOCTYPE html>
@@ -13,7 +16,7 @@ void setup() {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>**********************
+    <title>Document</title>
 </head>
 
 <body>
@@ -86,7 +89,7 @@ void setup() {
                 
             }
             function Mode_3() {
-                 fetch("Mode_2")
+                 fetch("Mode_3")
                 .then(()=>{})
                 .then(data=>{
                     
@@ -101,17 +104,31 @@ void setup() {
     )rawliteral");
   });
   server.on("/Mode_1", []() {
+    mode_3 = false;
     digitalWrite(led, HIGH);
+    digitalWrite(keyboard, HIGH);
     server.send(200, "text/plain", "LED is ON");
   });
 
   server.on("/Mode_2", []() {
+    mode_3 = false;
     digitalWrite(led, LOW);
+    digitalWrite(keyboard, LOW);
     server.send(200, "text/plain", "LED is OFF");
   });
+  server.on("/Mode_3", []() {
+    mode_3 = true;
+    server.send(200, "text/plain", "LED is OFF");
+  });
+
   server.begin();
 }
 
 void loop() {
+  if (mode_3) {
+    digitalWrite(keyboard, HIGH);
+    delay(500);
+    digitalWrite(keyboard, LOW);
+  }
   server.handleClient();
 }
